f1fd791629ef70ef5871c3c627b7867e0800f1fe
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-09-01 15:13:36 +0300

    feat: add layout of StringComponent

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import styles from "./string.module.css";
+
 
 export const StringComponent: React.FC = () => {
   return (
-    <SolutionLayout title="Строка">
-     
+    <SolutionLayout title="Строка" >
+     <div className={styles.container}>
+      <Input extraClass={styles.input} isLimitText maxLength={11}/>
+      <Button extraClass={styles.button} text='Развернуть'/>
+      </div>
     </SolutionLayout>
   );
 };
