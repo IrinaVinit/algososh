cbaee8dde41f6590c1b74f0677e9a4da41cbdf84
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-09-17 17:34:00 +0300

    feat: add layout sorting-page

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,85 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { RadioInput } from "../ui/radio-input/radio-input";
+import styles from "./sorting-page.module.css";
+import { Button } from "../ui/button/button";
+import { Direction } from "../../types/direction";
 
 export const SortingPage: React.FC = () => {
   return (
     <SolutionLayout title="Сортировка массива">
-
+      <div className={`${styles.grid} ${styles.container}`}>
+        <div className={`${styles.flex} ${styles.radioInputs}`}>
+          <RadioInput label="Выбор" checked />
+          <RadioInput label="Пузырек" />
+        </div>
+        <div className={`${styles.flex} ${styles.buttons}`}>
+          <Button extraClass={styles.button} sorting={Direction.Ascending} text='По возрастанию'/>
+          <Button extraClass={styles.button} sorting={Direction.Descending} text='По убыванию'/>
+        </div>
+        <div className={styles.newAr}>
+            <Button text='Новый массив' extraClass={styles.button} />
+        </div>
+        
+      </div>
     </SolutionLayout>
   );
 };
+
+// const partition = (arr: number[], start = 0, end = arr.length - 1): number => {
+//   debugger;
+//   const pivotValue = arr[end];
+//   console.log(arr[end]);
+
+//    let pivotIndex = start;
+//    console.log(pivotIndex);
+//    console.log(start);
+//  for (let i = start; i < end; i++) {
+//          if (arr[i] < pivotValue) {
+//            swap(arr, i,  pivotIndex);
+//             pivotIndex++;
+//      }
+//    }
+
+//    swap(arr, pivotIndex, end);
+//  console.log(arr);
+//   return pivotIndex;
+//  }
+
+//  const swap = (arr: number[], firstIndex: number, secondIndex: number): void => {
+//    const temp = arr[firstIndex];
+//    arr[firstIndex] = arr[secondIndex];
+//    arr[secondIndex] = temp;
+//  }
+
+//  const array = [7, -2, 4, 1, 6, 5, 0, -4, 2];
+//  console.log(partition(array, 0, array.length - 1));
+
+// export const selectionSort = (arr: number[]) => {
+
+//   debugger;
+//   const { length } = arr;
+//   for (let i = 0; i < length; i++) {
+//     let maxInd = i;
+//     for (let j=i+1; j < length; j++) {
+//       if(arr[j]>arr[maxInd]) {
+//         maxInd = j;
+
+//       }
+
+//     }
+//     swap(arr, i, maxInd);
+
+//   }
+//   return arr;
+// };
+
+//0, maxind=0,
+//1, 4>3, max = 1 (4)
+//2, 7>4, MAX = 2(7)
+//3, 2<7,
+//4, 1<7
+//5, 9>7, max=5(9)
+
+//swap(ch 0q b 5q - 3 b 9)
+// console.log(selectionSort([3,4,7,2,1,9,0]));
